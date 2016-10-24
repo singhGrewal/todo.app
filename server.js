c73d55ec232df1cca4d937fc7248ab65aa4a6f46
Tejveer <tejveer.pro@gmail.com>
2016-10-24 08:47:53 -0700

    add todos

--- server.js
+++ server.js
@@ -1,12 +1,56 @@
 var express = require('express');
 var app = express();
 
-var PORT = process.env.PORT || 3000
+var PORT = process.env.PORT || 3000;
 
-app.get('/' , function (req,res){
+var todos = [{
+	id: 1,
+	description: 'whats for lunch today',
+	completed: false
+},
+	{
+		id: 2,
+		description: ' pick up the food',
+		completed: false
+	},
+	{
+		id: 3,
+		description: ' was it goog ',
+		completed: true
+	}
+];
+
+app.get('/', function (req, res) {
 	res.send('Todo API');
 });
 
-app.listen(3000 , function(){
+// Get /todos
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) {
+	
+	var todoId = parseInt(req.params.id , 10);
+	var matchedTodo;
+	
+	todos.forEach(function (todo) {
+		if (todoId === todo.id) {
+			matchedTodo = todo;
+		}
+	});
+	
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+	
+	
+	// res.send('Asking for todo with id of ' + req.params.id)
+});
+
+
+app.listen(3000, function () {
 	console.log('Express listening on port ' + PORT + ' !');
-})
+});
